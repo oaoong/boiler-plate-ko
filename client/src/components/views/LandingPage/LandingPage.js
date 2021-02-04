fb7b96217b47294280b3b22863353219db6f48b4
oaoong <wjdwofkd12@naver.com>
2021-02-04 23:20:55 +0900

    로그인 회원가입 로그아웃

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,14 +1,37 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
-    axios.get("http://localhost:5000/api/hello").then((response) => {
+    axios.get("/api/hello").then((response) => {
       console.log(response);
     });
   }, []);
 
-  return <div>LandingPage1</div>;
+  const onClickHandler = () => {
+    axios.get(`api/users/logout`).then((response) => {
+      if (response.data.success) {
+        props.history.push("/login");
+      } else {
+        alert("로그아웃에 실패했습니다.");
+      }
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <h2>시작페이지</h2>
+      <button onClick={onClickHandler}>로그아웃</button>
+    </div>
+  );
 }
 
 export default LandingPage;
